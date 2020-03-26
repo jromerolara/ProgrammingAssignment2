6f0d7146dd4162e955eefd5fc299c96f9076b5df
Javier Romero <62362678+jromerolara@users.noreply.github.com>
2020-03-26 14:26:40 -0600

    Changes as per Programming Assignment 2
    Adapted the provided functions as requested @ RProgramming Course , Programming Assignment 2 and submit for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Used the previos versions of the provided functions and changed a few thing as per the assignment request
+## for the first one, I adapted it to process a Matrix instead of a vector, also changed the mean finction for inverse
 
-## Write a short comment describing this function
+## Based on the original provided makeVector and cacheMean functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # adapted the function in order to get a matrix as input
+  i <- NULL                                 # for clarity purposes redefined variable "m" for "i" to match the purpose "(i)nverse
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse # instead of setting the mean we are now using the inverse function
+  getinverse <- function() i                    # same as above, getting inverse of the matrix
+  list(set = set,
+       get = get,
+       setinverse = setinverse, # Here, also made changes to switch from mean to inverse function
+       getinverse = getinverse) # similar to above line, adapted to inverse the matrix
 }
 
 
-## Write a short comment describing this function
+## Made a few modifications to the original provided cacheMean function in order to retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ i <- x$getinverse() # made changes here to get inverse and assign to "i"
+  if (!is.null(i)) { # 
+          message("getting cached data")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...) # also changed this in order to use solve as per ?solve documentation (not quite sure if this works fine though
+  x$setinverse(i)  # also made changes here to adapt the functio to inverse the matrix (again, not quite sure how this works!
+  i
+}        
 }
